77692a933e6ab2d6a9d9f90cc0ad9f108e80e00a
Marina Silnova <marusia421@mail.ru>
2022-09-21 17:28:37 +0300

    add ex41

---
+++ ex41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите количество чисел, n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[n];
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0)
+        count++;
+    }
+Console.WriteLine("Исходный массив данных: [" + string.Join(", ", array) + "]");
+Console.WriteLine(count);
+
